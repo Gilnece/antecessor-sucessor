4a84739a0258074e64d7aaa6c19f7d38f0fb3385
Gilnece Menezes <82941591+Gilnece@users.noreply.github.com>
2024-08-29 12:38:13 -0300

    formulário
    index HTML contendo formulário simples para inserir numero desejado

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Antecessor e Sucessor</title>
+</head>
+<body>
+    <form action="calculo.php" method="post">
+        <label for="numero">Digite o numero desejado: </label>
+        <input type="numero" id="numero" name="numero">
+        <input type="submit" value="Calcular">
+    </form>
+ 
+</body>
+</html>
